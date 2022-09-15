ca49d583c9a0b227d3caa390f3fb2226bcdfb664
Lunde Chen <lundechen@shu.edu.cn>
2022-09-15 12:31:06 +0800

    init

--- sum_of_three.py
+++ sum_of_three.py
@@ -5,5 +5,5 @@ def sum3(first_number, second_number, third_number):
     Takes three numbers and returns the sum of those numbers
     """
     # $CHALLENGIFY_BEGIN
-    return first_number + second_number + third_number
+    return None
     # $CHALLENGIFY_END
